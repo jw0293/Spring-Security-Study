f1a5b9a8c7707c6e84e7efce1942254b4f667263
jw0293 <k38836919@gmail.com>
2022-07-19 15:32:06 +0900

    Refactor Config Because of WebSecurityConfigAdapter

---
+++ src/main/java/com/example/security/config/security/WebSecurityConfig.java
@@ -0,0 +1,113 @@
+package com.example.security.config.security;
+
+import com.example.security.handler.CustomLoginSuccessHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private final AuthenticationConfiguration configuration;
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    /**
+     * 빈 순환성은 아래 encoder에서 발생했었음
+     */
+    @Autowired
+    void configure(AuthenticationManagerBuilder builder,
+                   @Lazy BCryptPasswordEncoder encoder) throws Exception {
+        builder.userDetailsService(userDetailsService).passwordEncoder(encoder);
+    }
+
+
+    // 정적 자원에 대해서는 Security 설정을 적용하지 않음
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers();
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        AbstractAuthenticationProcessingFilter filter = new CustomAuthenticationFilter(authenticationManager());
+
+        filter.setFilterProcessesUrl("/user/login");
+        filter.setAuthenticationSuccessHandler(customLoginSuccessHandler());
+        filter.afterPropertiesSet();
+
+        http
+                .csrf().disable()
+                .httpBasic().disable();
+
+        http
+                .headers()
+                .frameOptions()
+                .sameOrigin();
+
+//        http
+//                .csrf().disable().authorizeRequests()
+//                // 토큰을 활용하는 경우 모든 요청에 대해 접근이 가능하도록 함
+//                .anyRequest().permitAll()
+//                .and()
+//                // 토큰을 활용하면 세션이 필요 없으므로 STATELESS로 설정하여 Session을 사용하지 않는다
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                // form 기반의 로그인에 대해 비활성화 한다.
+//                .formLogin()
+//                .disable()
+//                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+
+        http
+                .authorizeRequests()
+                .antMatchers("/h2-console/*","favicon.ico").permitAll()
+                .and()
+                // 토큰을 활용하면 세션이 필요 없으므로 STATELESS로 설정하여 Session을 사용하지 않는다
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                // form 기반의 로그인에 대해 비활성화 한다.
+                .formLogin()
+                .disable()
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CustomLoginSuccessHandler customLoginSuccessHandler(){
+        return new CustomLoginSuccessHandler();
+    }
+
+    @Bean @Lazy
+    public CustomAuthenticationProvider customAuthenticationProvider(){
+        return new CustomAuthenticationProvider(userDetailsService, bCryptPasswordEncoder());
+    }
+}
