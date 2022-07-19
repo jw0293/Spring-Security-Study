36fa628cd3c70ab866f257a43c532e01f226b212
jw0293 <k38836919@gmail.com>
2022-07-19 15:34:23 +0900

    Refactor Config Because of WebSecurityConfigAdapter

--- src/main/java/com/example/security/config/security/WebSecurityConfig.java
+++ src/main/java/com/example/security/config/security/WebSecurityConfig.java
@@ -67,19 +67,6 @@ public class WebSecurityConfig {
                 .frameOptions()
                 .sameOrigin();
 
-//        http
-//                .csrf().disable().authorizeRequests()
-//                // 토큰을 활용하는 경우 모든 요청에 대해 접근이 가능하도록 함
-//                .anyRequest().permitAll()
-//                .and()
-//                // 토큰을 활용하면 세션이 필요 없으므로 STATELESS로 설정하여 Session을 사용하지 않는다
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                // form 기반의 로그인에 대해 비활성화 한다.
-//                .formLogin()
-//                .disable()
-//                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
-
         http
                 .authorizeRequests()
                 .antMatchers("/h2-console/*","favicon.ico").permitAll()
