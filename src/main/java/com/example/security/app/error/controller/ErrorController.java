3fd4db645be214403d070d5f0e10edcf8f16ef95
jw0293 <k38836919@gmail.com>
2022-07-25 15:00:40 +0900

    Refactor Project

---
+++ src/main/java/com/example/security/app/error/controller/ErrorController.java
@@ -0,0 +1,17 @@
+package com.example.security.app.error.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/error")
+public class ErrorController {
+
+    @GetMapping("/unauthorized")
+    public ResponseEntity<Void> unauthorized(){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+}
